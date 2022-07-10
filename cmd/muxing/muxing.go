4653882f8f81ffc122f4893ddbd095a0f67b89e4
IliaBadz <badzaghua.ilia@gmail.com>
2022-07-10 16:07:08 +0400

    add: homework-10

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -10,16 +11,42 @@ import (
 	"github.com/gorilla/mux"
 )
 
-/**
-Please note Start functions is a placeholder for you to start your own solution.
-Feel free to drop gorilla.mux if you want and use any other solution available.
+func hello(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	response := fmt.Sprintf("Hello, %s!", vars["PARAM"])
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, response)
+}
 
-main function reads host/port from env just for an example, flavor it following your taste
-*/
+func bad(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+}
 
-// Start /** Starts the web server listener on given host and port.
+func data(w http.ResponseWriter, r *http.Request) {
+	body, _ := io.ReadAll(r.Body)
+	fmt.Println(body)
+	response := fmt.Sprintf("I got message:\n%s", body)
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, response)
+
+}
+
+func headers(w http.ResponseWriter, r *http.Request) {
+	firstVal, _ := strconv.Atoi(r.Header.Get("a"))
+	secondVal, _ := strconv.Atoi(r.Header.Get("b"))
+
+	resSum := firstVal + secondVal
+	
+	w.Header().Set("a+b", fmt.Sprintf("%d", resSum))
+}
+
 func Start(host string, port int) {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/name/{PARAM}", hello).Methods("GET")
+	router.HandleFunc("/bad", bad).Methods("GET")
+	router.HandleFunc("/data", data).Methods("POST")
+	router.HandleFunc("/headers", headers).Methods("POST")
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
@@ -30,7 +57,12 @@ func Start(host string, port int) {
 //main /** starts program, gets HOST:PORT param and calls Start func.
 func main() {
 	host := os.Getenv("HOST")
+	if host == "" {
+		host = "localhost"
+	}
+	fmt.Println(host)
 	port, err := strconv.Atoi(os.Getenv("PORT"))
+	fmt.Println(port)
 	if err != nil {
 		port = 8081
 	}
